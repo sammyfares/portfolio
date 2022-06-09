7ad526946161b424e3b9011e63c4032aa9971ef3
User Name <user@email.com>
2022-06-09 14:09:08 -0400

    remove border

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ let projectData = [
       deployment: 'https://tic-taac-toe-sammy.netlify.app',
       image: '/assets/back.jpg',
       github: 'https://github.com/sammyfares/ttt-weekend.git',
-      description: 'A React application that utilizes the Animal Crossing API to fetch data for several resources.'
+      description: 'Classic Game'
     },]
     
