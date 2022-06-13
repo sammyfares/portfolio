483c829712a5bae2b4326ed36aa7df5dcd35c287
User Name <user@email.com>
2022-06-13 19:10:11 -0400

    complete import

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-let projectData = [
-    {
-      title: 'Tic-Tac-Toe',
-      deployment: 'https://tic-taac-toe-sammy.netlify.app',
-      image: '/assets/back.jpg',
-      github: 'https://github.com/sammyfares/ttt-weekend.git',
-      description: 'Classic Game'
-    },]
-    
+// import data here
+import { projectData } from "./project-data.js"
+
+const cardContainer = document.getElementById('card-container')
+
+
+
+let projectMarkup = projectData.map(project =>
+  `
+    <div class="card text-center" style="width: 18rem;">
+      <img src="${project.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${project.title}</h5>
+        <p class="card-text">${project.description}</p>
+        <div>
+          <a href="${project.github}" class="btn btn-primary">GitHub</a>
+          <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+        </div>
+      </div>
+    </div>
+  `  
+).join('')
+
+console.log(projectMarkup)
+cardContainer.innerHTML = projectMarkup
+
+
+
+
---
+++ js/project-data.js
@@ -0,0 +1,14 @@
+
+let projectData = [
+    {
+      title: 'Tic-Tac-Toe',
+      deployment: 'https://tic-taac-toe-sammy.netlify.app',
+      image: '/assets/back.jpg',
+      github: 'https://github.com/sammyfares/ttt-weekend.git',
+      description: 'Classic Game'
+    },]
+    
+    export {
+      projectData
+    }
+    
